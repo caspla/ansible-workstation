8011d7191b559ac58201b8b52696d11f8f8ada82
Pascal <git@caspla.net>
2018-08-11 01:12:09 +0200

    misc stuff

--- run-as-root.sh
+++ run-as-root.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+
+echo 'deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main' > /etc/apt/sources.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 apt update
 apt install -y ansible
 ansible-playbook -K site.yml
