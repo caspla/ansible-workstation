c42e28d5cdd08b0bae8122bc11b2ca85fc17a0ae
caspla <git@caspla.net>
2018-09-10 00:10:49 +0200

    fix

--- run-as-root.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo add-apt-repository "deb [arch=amd64] http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main"
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
-apt update
-apt install -y ansible
-ansible-playbook -K site.yml
